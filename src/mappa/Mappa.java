c20661d3553b3207e69632be6d8017f9b1e461e5
Raul Iscu Lacatusu <isculacatusu.2178309@studenti.uniroma1.it>
2025-03-21 14:12:59 +0100

    Formatted Mappa.java

--- src/mappa/Mappa.java
+++ src/mappa/Mappa.java
@@ -24,29 +24,41 @@ public class Mappa
 {
     public static void main(String[] args)
     {
-        TileFactoryInfo info = new OSMTileFactoryInfo("Cazzo","https://a.tile.openstreetmap.fr/hot/");
+    	// Dichiarazione del tileset da cui caricare la mappa
+        TileFactoryInfo info = new OSMTileFactoryInfo("Mappa","https://a.tile.openstreetmap.fr/hot/");
         DefaultTileFactory tileFactory = new DefaultTileFactory(info);
 
-        File cacheDir = new File(System.getProperty("user.home") + File.separator + ".jxmapviewer2");
-        tileFactory.setLocalCache(new FileBasedLocalCache(cacheDir, false));
-
+        
+        // Istanziamento della mappa, indicazione per il numero di thread utilizzati per renderizzare
         final JXMapViewer mapViewer = new JXMapViewer();
         mapViewer.setTileFactory(tileFactory);
+        
         tileFactory.setThreadPoolSize(8);
+        
+        
+        // Implementazione di una cache per la mappa
+        File cacheDir = new File(System.getProperty("user.home") + File.separator + ".jxmapviewer2");
+        tileFactory.setLocalCache(new FileBasedLocalCache(cacheDir, false));
+        
+        
+        // Specifica della posizione e dello zoom da visualizzare all'avvio
         GeoPosition Roma = new GeoPosition(41.90, 12.49);
-
-        mapViewer.setZoom(7);
         mapViewer.setAddressLocation(Roma);
+        mapViewer.setZoom(7);
 
+        
+        // Dichiarazione e inizializzazione del sistema di gestione input per il mouse
         MouseInputListener Mouse = new PanMouseInputListener(mapViewer);
-        ZoomMouseWheelListenerCursor Wheel= new ZoomMouseWheelListenerCursor(mapViewer);
+        ZoomMouseWheelListenerCursor Wheel = new ZoomMouseWheelListenerCursor(mapViewer);
+        
         mapViewer.addMouseListener(Mouse);
         mapViewer.addMouseMotionListener(Mouse);
         mapViewer.addMouseWheelListener(Wheel);
 
         
-        final JFrame frame = new JFrame("Damose App Trasporti Roma");
-        final JLabel label = new JLabel("Fratello cosa");
+        // Costruzione della finestra che contiene la mappa
+        final JFrame frame = new JFrame("Damose");
+        
         frame.setLayout(new BorderLayout());
         frame.add(mapViewer);
         frame.setSize(800, 600);
