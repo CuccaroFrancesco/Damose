21835596091758b244e6898488d20f75397dd459
Raul Iscu Lacatusu <isculacatusu.2178309@studenti.uniroma1.it>
2025-03-23 16:42:46 +0100

    Formatted Schermo.java (FIX ERROR)

--- src/mappa/Schermo.java
+++ src/mappa/Schermo.java
@@ -1,5 +1,4 @@
 package mappa;
-
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
@@ -23,69 +22,89 @@ import org.jxmapviewer.viewer.DefaultTileFactory;
 import org.jxmapviewer.viewer.GeoPosition;
 import org.jxmapviewer.viewer.TileFactoryInfo;
 
+
 public class Schermo {
 
     // Variabili per la classe necessarie per accedere sempre
     private static DefaultTileFactory tileFactory;
     private static TileFactoryInfo info;
 
-    // Creo la schermata di base dell'applicazione
-    public static JFrame Schermata() 
-    {
+    
+    // Creazione della schermata di base dell'applicazione
+    public static JFrame Schermata() {
+    	
         final JFrame frame = new JFrame("Damose App Trasporti");
+        
         frame.getContentPane().setLayout(new BorderLayout());
         frame.setSize(1080, 720);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        
         return frame;  
     }
     
+    
     public static void main(String[] args) {
-    	// Serve a poter eseguire il codice dopo altre funzionalità in modo da evitare rallentamenti
+    	
+    	// Serve a poter eseguire il codice dopo altre funzionalità, in modo da evitare rallentamenti
         EventQueue.invokeLater(new Runnable() {
+        	
             public void run() {
+            	
                 try {
-                	//Creo il frame tramite la funzione dichiarata sopra
+                	
+                	// Creazione della schermata mediante il metodo dichiarato sopra
                     JFrame frame = Schermata();  
 
+                    
                     // Inizializzazione della mappa con quella di default 
                     info = new OSMTileFactoryInfo("Mappa", "https://a.tile.openstreetmap.fr/hot/");
                     tileFactory = new DefaultTileFactory(info);
+                    
                     
                     // Implementazione di una cache per la mappa 
                     File cacheDir = new File(System.getProperty("user.home") + File.separator + ".jxmapviewer2");
                     tileFactory.setLocalCache(new FileBasedLocalCache(cacheDir, false));
                     
-                    // Istanziamento della mappa, indicazione per il numero di thread utilizzati per renderizzare
+                    
+                    // Istanziamento del visualizzatore della mappa, indicazione per il numero di thread utilizzati per renderizzare
                     final JXMapViewer mapViewer = new JXMapViewer();
                     mapViewer.setTileFactory(tileFactory);
                     tileFactory.setThreadPoolSize(8);
+                    
                     
                     // Specifica della posizione e dello zoom da visualizzare all'avvio
                     GeoPosition Roma = new GeoPosition(41.90, 12.49);
                     mapViewer.setAddressLocation(Roma);
                     mapViewer.setZoom(7);
 
-                    // Creo l'elemento di scelta per selezionare il tipo di mappa
+                    
+                    // Creazione dell'elemento di scelta per selezionare il tipo di mappa
                     JComboBox<String> scelta = new JComboBox<>();
                     scelta.setModel(new DefaultComboBoxModel<>(new String[] { "Normale", "Satellite", "Mista" }));
 
+                    
                     // Aggiungo un evento che al cambio di selezione modifica il tipo di mappa
                     scelta.addActionListener(new ActionListener() {
+                    	
                         public void actionPerformed(ActionEvent e) {
+                        	
                             int selectedIndex = scelta.getSelectedIndex();
+                            
                             switch (selectedIndex) {
                                 case 0:  // Normale
                                     info = new OSMTileFactoryInfo("Mappa", "https://a.tile.openstreetmap.fr/hot/");
                                     break;
+                                    
                                 case 1:  // Satellite
                                     info = new VirtualEarthTileFactoryInfo(VirtualEarthTileFactoryInfo.SATELLITE);
                                     break;
+                                    
                                 case 2:  // Mista
                                     info = new VirtualEarthTileFactoryInfo(VirtualEarthTileFactoryInfo.HYBRID);
                                     break;
                             }
                             
-                            // Aggiorno la mappa e reimposto lo zoom (purtroppo dava problemi lo zoom, ma la posizione rimane)
+                            // Aggiornamento della mappa e reimpostazione dello zoom (purtroppo dava problemi lo zoom, ma la posizione rimane)
                             tileFactory = new DefaultTileFactory(info);
                             mapViewer.setTileFactory(tileFactory);
                             mapViewer.setZoom(7);
@@ -93,19 +112,22 @@ public class Schermo {
                         }
                     });
                     
+                    
                     // Dichiarazione e inizializzazione del sistema di gestione input per il mouse
                     MouseInputListener Mouse = new PanMouseInputListener(mapViewer);
                     ZoomMouseWheelListenerCursor Wheel = new ZoomMouseWheelListenerCursor(mapViewer);
+                    
                     mapViewer.addMouseListener(Mouse);
                     mapViewer.addMouseMotionListener(Mouse);
                     mapViewer.addMouseWheelListener(Wheel);
                     
-                    // Aggiungo il "selezionatore" del tipo di mappa e la mappa alla schermata
+                    // Aggiunta alla schermata del "selezionatore" del tipo di mappa e della mappa stessa
                     frame.getContentPane().add(scelta, BorderLayout.NORTH);
                     frame.getContentPane().add(mapViewer, BorderLayout.CENTER);
                     frame.setVisible(true); 
+                    
                 } catch (Exception e) {
-                    e.printStackTrace(); // Stampo un eventuale errore a schermo (non dovrebbe accadere, però u never know)
+                    e.printStackTrace(); // Stampa un eventuale errore a schermo (non dovrebbe accadere, però u never know)
                 }
             }
         });
